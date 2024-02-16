8915a580cc2a6d639bb2e75e1e77458b1186c820
Kevin Muthami <153747395+MuthamiKN@users.noreply.github.com>
2024-02-16 16:08:05 +0300

    update

--- To-Do list app/app.py
+++ To-Do list app/app.py
@@ -1,46 +1,56 @@
 import tkinter as tk
-from tkinter import *
-import tkinter.messagebox
+from tkinter import messagebox
 
 def add_task():
-    text=entrybox.get()
+    text = entrybox.get()
     entrybox.delete(0, tk.END)
-    if text=="":
-        tk.messagebox.showwarning(title="Warning!",message="Please Enter some Text")
+    if text == "":
+        messagebox.showwarning(title="Warning!", message="Please Enter a new task.")
     else:
-        listbox.insert(END ,text)
-        tk.messagebox.showinfo("Info", "Task added to the list.")
+        listbox.insert(tk.END, text)
+        messagebox.showinfo("Info", "Task added to the list.")
         
-window=tk.Tk()
+        items.append(text)  # Store the item in a list
+        update_numbers()  # Update the numbering of items
+
+def update_numbers():
+    listbox.delete(0, tk.END)  # Clear the listbox
+    for i, item in enumerate(items, start=1):
+        listbox.insert(tk.END, f"{i}. {item}")
+
+window = tk.Tk()
 window.title("To-Do list App")
 window.geometry('450x500')
-window.resizable(0,0)
+window.resizable(0, 0)
 
-frame_app=Frame(window)
-frame_app.place(x=50,y=50)
-listbox=Listbox(frame_app, height=15,width=50)
+frame_app = tk.Frame(window)
+frame_app.place(x=50, y=50)
+
+listbox = tk.Listbox(frame_app, height=15, width=50)
 listbox.pack(side=tk.LEFT)
 
-scrollbar=Scrollbar(frame_app)
+scrollbar = tk.Scrollbar(frame_app)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
+tasklabel = tk.Label(window, text="Add new task here.")
+tasklabel.place(x=5, y=5)
 
-tasklable=tk.Label(window, text="Add new task here.")
-tasklable.place(x=5,y=5)
+entrybox = tk.Entry(window)
+entrybox.place(x=15, y=25)
+entrybutton = tk.Button(window, width=15, text="Add new task", command=add_task)
+entrybutton.place(x=150, y=20)
 
-entrybox=tk.Entry(window)
-entrybox.place(x=15,y=25)
-entrybutton=tk.Button(window, width=15, text="Add new task", command=add_task)
-entrybutton.place(x=150,y=20)
+items = []  # List 
 
-update_button=tk.Button(window, width=15, text="Update task")
-update_button.place(x=15,y=300)
+update_button = tk.Button(window, width=15, text="Update task")
+update_button.place(x=15, y=300)
 
-delete_button=tk.Button(window, width=15, text="Delete task")
-delete_button.place(x=15,y=350)
+delete_button = tk.Button(window, width=15, text="Delete task")
+delete_button.place(x=15, y=350)
 
-mark_button=tk.Button(window, width=15, text="Mark task as done")
+mark_button = tk.Button(window, width=15, text="Mark task as done")
 mark_button.place(x=15, y=400)
+
 window.mainloop()
